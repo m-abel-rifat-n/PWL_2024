1aef122b746d117966ab3c033531999d932d1e27
Abel <evilfighterthirdacc@gmail.com>
2024-09-04 08:38:11 +0700

    modifikasi jobsheet 2 pratikum 1 basic routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,44 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function () {
+ return 'Hello World';
+});
+
+Route::get('/world', function () {
+ return 'World';
+});
+
+Route::get('/about', function () {
+    return '2241760045/Abel';
+   });
+
+   Route::get('/', function () {
+    return 'Selamat Datang';
+   });
+
+//    Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya '.$name;
+//     });
+    
+    Route::get('/posts/{post}/comments/{comment}', function
+    ($postId, $commentId) {
+     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+    
+    Route::get('/articles/{id}', function($id) {
+        return 'Halaman Artikel dengan ID ' . $id;
+    });
+
+    // Route::get('/user/{name?}', function ($name=null) {
+    //     return 'Nama saya '.$name;
+    // });
+
+    Route::get('/user/{name?}', function ($name='John') {
+        return 'Nama saya '.$name;
+        });
+        
+    
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +52,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Welcome');
 });
